e5b9cab622794548e43c5dc468016bc4d46021ca
Maria3203 <maria.diaconescu46@yahoo.com>
2026-03-23 19:21:56 +0200

    Tema 1: remediere eroare

--- src/Example.cpp
+++ src/Example.cpp
@@ -2,7 +2,7 @@
 // #include "../include/Example.h"
 // This also works if you do not want `../`, but some editors might not like it
 // #include "Example.h"
-
+//
 // void Example::f() const {
 //     std::cout << "private function f: " << x << "\n";
 // }
